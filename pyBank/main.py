a69a7408d49913719f06d91283143c4531be0f52
Tsebaot <Tsebaotsol@gmail.com>
2022-12-17 23:18:43 -0500

    revised python files

--- pyBank/main.py
+++ pyBank/main.py
@@ -10,10 +10,12 @@ import csv
 #Lists to store the Date and Profit/Losses data
 date = []
 profit_losses = []
+change_in_profit = []
 
 #Set path for the file
 csv_path = os.path.join("Resources","budget_data.csv")
 
+previous_profit = 0
 #open the file 
 with open(csv_path) as csvfile:
     
@@ -22,30 +24,52 @@ with open(csv_path) as csvfile:
 
     # Read the header row first
     csv_header = next(csvreader)
-
-    # append the content of each row in the date and Ppofit_losses list 
+    
+    # Read through each row of data after the header 
     for anyrow in csvreader:
+        
+        # append the date from the data to the data list
         date.append(anyrow[0])
-        profit_losses.append(anyrow[1])
+        
+        profit_losses.append(float(anyrow[1]))
+        
+        csv_header = next(csvreader)
+        # convert the Profit/Losses data into a float and append it to the profit_losses list
+        change_in_profit.append(float(anyrow[1]) - previous_profit)
+        previous_profit = float(anyrow[1])
 
-#calulate and print the total number months        
-print("\nTotal Months: " + str(len(date)))
 
-# add the profit and losses
+#calulate and print the total number months & print the result       
+total_months = len(date)
+print("\nTotal Months: " + str(total_months))
+
+# calculate the total profit and losses & print the result
 total = sum(profit_losses)
 print("\nTotal: $"+str(total))
 
-#
-average = round(total/len(profit_losses),2)
-print("\nAverage Change: $" + average)
 
-#
-greatest_increase = max(profit_losses)
-x = profit_losses.index(greatest_increase)
-print("\nGreatest Increase in Profits: " + date[x] + "($" + greatest_increase + ")")
+#the average change in profit is calculated by dividing the total change with the total number
+#change_in_profit.remove(0)
+total_change_in_profit = sum(change_in_profit)
+#print(total_change_in_profit)
+average = round(total_change_in_profit/(len(date)-1))
 
-#   
-greatest_decrease = min(profit_losses)
-y = profit_losses.index(greatest_decrease)
-print("\nGreatest Decrease in Profits: " + date[y] + "($" + greatest_decrease + ")")
+#print the average change in profit
+print("\nAverage Change: $" + str(average))
+
+
+# determine the maximum change in profit 
+greatest_increase = max(change_in_profit)
+# determine the index value of the maximum change in profit 
+x = change_in_profit.index(greatest_increase)
+# print the date & value of for the maximum change in profit 
+print("\nGreatest Increase in Profits: " + date[x] + " ($" + str(greatest_increase) + ")")
+
+
+# determine the biggest loss in profit  
+greatest_decrease = min(change_in_profit)
+# determine the index value of the biggest loss in profit
+y = change_in_profit.index(greatest_decrease)
+# print the date & value of for the biggest loss in profit
+print("\nGreatest Decrease in Profits: " + date[y] + " ($" + str(greatest_decrease) + ")")
 
--- pyPoll/main.py
+++ pyPoll/main.py
@@ -23,15 +23,16 @@ with open(csv_path) as csvfile:
     # Read the header row first
     csv_header = next(csvreader)
 
-    # append the content of each row in the ballotId and candidates list
+    # Read through each row of data after the header
     for anyrow in csvreader:
+        
+        # append the content of each row to the ballotId and candidates list
         ballotId.append(anyrow[0])
         candidates.append(anyrow[2])
 
 
 #calulate the total number of casted vote & print the result
 total_vote_cast = len(ballotId)
-
 print("\n Total Votes: " + str(total_vote_cast))        
 
 print("\n---------------------------------------------------------------------")
@@ -43,25 +44,31 @@ Charles_Casper_Stockham_vote = candidates.count("Charles Casper Stockham")
 # calculate the percentage of vote Charles Casper Stockham got from the total
 Charles_Casper_Stockham_perentage = round((Charles_Casper_Stockham_vote/total_vote_cast * 100),3)
 
+# print the percentage and total number of vote Charles Casper Stockham got 
 print("\n Charles Casper Stockham: " + str(Charles_Casper_Stockham_perentage) + "% (" + str(Charles_Casper_Stockham_vote) + ")")        
 
 
+#--------------------------------------------------------------------------------------------------
 # calculate the total number of vote Diana DeGette recieved
 Diana_DeGette_vote = candidates.count("Diana DeGette")
 
 # calculate the percentage of vote Diana DeGette got from the total
 Diana_DeGette_perentage = round((Diana_DeGette_vote/total_vote_cast * 100),3)
 
+# print the percentage and total number of vote Diana DeGette got 
 print("\n Diana DeGette: " + str(Diana_DeGette_perentage) + "% (" + str(Diana_DeGette_vote) + ")")
 
 
-# calculate the total number of vote Raymon Anthony Doane recieved
+#--------------------------------------------------------------------------------------------------
+# the total number of vote Raymon Anthony Doane recieved
 Raymon_Anthony_Doane_vote = candidates.count("Raymon Anthony Doane")
 
 # calculate the percentage of vote Raymon Anthony Doane got from the total
 Raymon_Anthony_Doane_perentage = round((Raymon_Anthony_Doane_vote/total_vote_cast * 100),3)
 
+# print the percentage and total number of vote Raymon Anthony Doane got 
 print("\n Raymon Anthony Doane: " + str(Raymon_Anthony_Doane_perentage) + "% (" + str(Raymon_Anthony_Doane_vote) + ")")  
+
 
 print("\n---------------------------------------------------------------------")
 
